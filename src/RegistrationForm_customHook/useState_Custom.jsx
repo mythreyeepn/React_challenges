569d38b892539181720ba016e4147fae17ed6662
Mythreyee Pingala <mythreyeepn@gmail.com>
2024-02-25 14:27:32 -0600

    Error corrections

--- src/RegistrationForm_customHook/useState_Custom.jsx
+++ src/RegistrationForm_customHook/useState_Custom.jsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 
 export default function UseMyState(initialVal, errors) {
   const [finalVal, setFinalVal] = useState(initialVal);
-  const [errors, setErrors] = useState(errors);
+  const [Finalrrors, setFinalErrors] = useState(errors);
   function sethi({ name, value }) {
     setFinalVal((prevFormObj) => ({
       ...prevFormObj,
       [name]: value + "hi",
     }));
   }
-  return [finalVal, setFinalVal, errors, setErrors];
+  return [finalVal, setFinalVal, errors, setFinalErrors];
 }
